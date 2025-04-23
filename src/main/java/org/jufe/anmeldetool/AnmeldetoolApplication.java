c7e21b2982110f828f8ff33530df21e4009c8b4d
Lukas Marsch <lmarsch@vater-gruppe.de>
2025-04-23 21:14:07 +0200

    init

---
+++ src/main/java/org/jufe/anmeldetool/AnmeldetoolApplication.java
@@ -0,0 +1,13 @@
+package org.jufe.anmeldetool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnmeldetoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnmeldetoolApplication.class, args);
+	}
+
+}
