6210cdf4d1e8b926390dcb086b471fd9eff3e6fc
Lukas Marsch <lmarsch@vater-gruppe.de>
2025-05-08 22:35:06 +0200

    Controller init

---
+++ src/main/java/org/jufe/anmeldetool/controller/open/PublicController.java
@@ -0,0 +1,11 @@
+package org.jufe.anmeldetool.controller.open;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/app")
+public class PublicController {
+}
