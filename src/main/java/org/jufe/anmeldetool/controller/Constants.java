dee076e1ff65a6d721a90a5ee41655d0f7cf6620
Lukas Marsch <lmarsch@vater-gruppe.de>
2025-05-14 22:42:30 +0200

    entities success, message, redirect nach Anmeldung

--- src/main/java/org/jufe/anmeldetool/controller/Constants.java
+++ src/main/java/org/jufe/anmeldetool/controller/Constants.java
@@ -1,12 +1,23 @@
 package org.jufe.anmeldetool.controller;
 
 public class Constants {
-    private Constants(){}
+
+    private Constants() {}
+
+    public static final String ENTITY_SUCCESS = "success";
+
+    public static final String ENTITY_MESSAGE = "message";
 
     public static final String ENTITY_EVENT = "event";
+
     public static final String ENTITY_ANMELDUNG = "anmeldung";
+    
+    public static final String MESSAGE_ANMELDUNG_SAVED = "Anmeldung erfolgreich gespeichert";
+
+    public static final String REDIRECT_ANMELDUNG = "redirect:/anmeldung";
 
     public static final String REDIRECT_LOGIN = "redirect:/login";
+
     public static final String REDIRECT_SHUTTLE = "redirect:/app/shuttle";
 
     public static final String VIEW_ANMELDE_FORMULAR = "anmeldeFormular";
