fb0d3fbd7cb9a93b16230bfcdaf7ce2894bc10c4
Lukas Marsch <lmarsch@vater-gruppe.de>
2025-05-26 23:30:46 +0200

    shuttle 3 & 4

--- src/main/java/org/jufe/anmeldetool/service/EventService.java
+++ src/main/java/org/jufe/anmeldetool/service/EventService.java
@@ -47,6 +47,8 @@ public class EventService extends BaseService<Event> {
                        .von(LocalDate.of(2018, 1, 1))
                        .bis(LocalDate.of(2018, 1, 3))
                        .build();
+
+        var heskem = new PostAdresse("L3125", "Busbahnhof", "35085", "Heskem");
         Shuttle shuttle1hin = shuttleService.createShuttle(e, null, "");
         Shuttle shuttle1rueck = shuttleService.createShuttle(e, null, "");
         Shuttle shuttle2hin = shuttleService.createShuttle(e, null, "");
@@ -92,6 +94,48 @@ public class EventService extends BaseService<Event> {
         halt2rueck.add(new Halt(new PostAdresse("Am Bahnhof", "1B", "33602", "Bielefeld"), LocalTime.of(15, 45), 17.5, shuttle2rueck));
         halt2rueck.add(new Halt(new PostAdresse("Bahnhofstraße", "29", "33102", "Paderborn"), LocalTime.of(14, 30), 15, shuttle2rueck));
         halt2rueck.add(new Halt(new PostAdresse("Knallhütter Straße", "43", "34225", "Baunatal"), LocalTime.of(12, 30), 10, shuttle2rueck));
+
+        halt3hin.add(new Halt(new PostAdresse("Bertha-von-Suttner-Weg", "1", "81245", "München"), LocalTime.of(9, 30), 32.5d, shuttle3hin));
+        halt3hin.add(
+                new Halt(new PostAdresse("Filderbahnstraße/Klinkerstraße", "", "70771", "Echterdingen"), LocalTime.of(12, 00), 27.5d, shuttle3hin));
+        halt3hin.add(new Halt(new PostAdresse("A8 Ausfahrt Hohenäckerallee", "", "75177", "Pforzheim"), LocalTime.of(13, 00), 25d, shuttle3hin));
+        halt3hin.add(new Halt(new PostAdresse("S-Bahnhof Pfaffengrund", "", "69123", "Heidelberg"), LocalTime.of(14, 15), 22.5d, shuttle3hin));
+        halt3hin.add(new Halt(new PostAdresse("Robert-Koch-Straße", "1", "64331", "Weiterstadt"), LocalTime.of(15, 15), 20d, shuttle3hin));
+        halt3hin.add(new Halt(new PostAdresse("Max-Holder-Straße", "2", "60437", "Frankfurt am Main"), LocalTime.of(16, 15), 15d, shuttle3hin));
+        halt3hin.add(new Halt(new PostAdresse("Liebigstraße", "12", "35390", "Gießen"), LocalTime.of(16, 45), 10d, shuttle3hin));
+
+        halt3rueck.add(new Halt(new PostAdresse("Liebigstraße", "12", "35390", "Gießen"), LocalTime.of(12, 30), 10d, shuttle3rueck));
+        halt3hin.add(new Halt(new PostAdresse("Max-Holder-Straße", "2", "60437", "Frankfurt am Main"), LocalTime.of(13, 00), 15d, shuttle3rueck));
+        halt3hin.add(new Halt(new PostAdresse("Robert-Koch-Straße", "1", "64331", "Weiterstadt"), LocalTime.of(14, 00), 20d, shuttle3rueck));
+        halt3hin.add(new Halt(new PostAdresse("S-Bahnhof Pfaffengrund", "", "69123", "Heidelberg"), LocalTime.of(15, 00), 22.5d, shuttle3rueck));
+        halt3hin.add(new Halt(new PostAdresse("A8 Ausfahrt Hohenäckerallee", "", "75177", "Pforzheim"), LocalTime.of(16, 15), 25d, shuttle3rueck));
+        halt3hin.add(
+                new Halt(new PostAdresse("Filderbahnstraße/Klinkerstraße", "", "70771", "Echterdingen"), LocalTime.of(17, 15), 27.5d, shuttle3rueck));
+        halt3hin.add(new Halt(new PostAdresse("Bertha-von-Suttner-Weg", "1", "81245", "München"), LocalTime.of(19, 45), 32.5d, shuttle3rueck));
+
+        halt4hin.add(new Halt(new PostAdresse("Vetschauer Straße", "70", "03048", "Cottbus"), LocalTime.of(7, 45), 32.5d, shuttle4hin));
+        halt4hin.add(new Halt(new PostAdresse("Hildegard-Knef-Platz", "", "10829", "Berlin"), LocalTime.of(9, 30), 30d, shuttle4hin));
+        halt4hin.add(new Halt(new PostAdresse("Bushaltestelle Hans-Albers-Straße", "", "14480", "Potsdam Südost"), LocalTime.of(10, 00), 25d,
+                shuttle4hin));
+        halt4hin.add(new Halt(new PostAdresse("(TotalEnergies Autothof an der A9) Sandbreite", "1", "06869", "Coswig"), LocalTime.of(11, 00), 22.5d,
+                shuttle4hin));
+        halt4hin.add(
+                new Halt(new PostAdresse("Bushaltestelle am Bahnhof Leipzig/Halle Flughafen", "", "04435", "Schkeuditz"), LocalTime.of(11, 45), 17.5d,
+                        shuttle4hin));
+        halt4hin.add(new Halt(new PostAdresse("Busbahnhof Sangerhausen", "", "06526", "Sangerhausen"), LocalTime.of(14, 00), 15d, shuttle4hin));
+        halt4hin.add(new Halt(new PostAdresse("Bahnhofsplatz", "", "99734", "Nordhausen"), LocalTime.of(15, 15), 12.5d, shuttle4hin));
+
+        halt4rueck.add(new Halt(new PostAdresse("Bahnhofsplatz", "", "99734", "Nordhausen"), LocalTime.of(12, 00), 12.5d, shuttle4rueck));
+        halt4rueck.add(new Halt(new PostAdresse("Busbahnhof Sangerhausen", "", "06526", "Sangerhausen"), LocalTime.of(14, 30), 15d, shuttle4rueck));
+        halt4rueck.add(
+                new Halt(new PostAdresse("Bushaltestelle am Bahnhof Leipzig/Halle Flughafen", "", "04435", "Schkeuditz"), LocalTime.of(17, 30), 17.5d,
+                        shuttle4rueck));
+        halt4rueck.add(new Halt(new PostAdresse("(TotalEnergies Autohof an der A9) Sandbreite", "1", "06869", "Coswig"), LocalTime.of(18, 15), 22.5d,
+                shuttle4rueck));
+        halt4rueck.add(new Halt(new PostAdresse("Vetschauer Straße", "70", "03048", "Cottbus"), LocalTime.of(19, 15), 32.5d, shuttle4rueck));
+        halt4rueck.add(new Halt(new PostAdresse("Hildegard-Knef-Platz", "", "10829", "Berlin"), LocalTime.of(19, 45), 30d, shuttle4rueck));
+        halt4rueck.add(new Halt(new PostAdresse("Bushaltestelle Hans-Albers-Straße", "", "14480", "Potsdam Südost"), LocalTime.of(21, 30), 25d,
+                shuttle4rueck));
 
         repo.save(e);
         return e;
