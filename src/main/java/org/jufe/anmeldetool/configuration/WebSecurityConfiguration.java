24cf2e727f426c38104fee75d509d8fd32fc5075
Lukas Marsch <lmarsch@vater-gruppe.de>
2025-05-30 10:04:06 +0200

    Doppelte Config entfernen

--- src/main/java/org/jufe/anmeldetool/configuration/WebSecurityConfiguration.java
+++ src/main/java/org/jufe/anmeldetool/configuration/WebSecurityConfiguration.java
@@ -1,4 +1,4 @@
-package org.jufe.anmeldetool.security;
+package org.jufe.anmeldetool.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
